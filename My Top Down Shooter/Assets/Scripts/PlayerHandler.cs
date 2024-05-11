a84b9af9c3602b38d9123b7ec26dac1b02240ccd
Prof <pedagogicomyrobotjoinville@gmail.com>
2024-05-11 12:02:03 -0300

    Aula 21

--- My Top Down Shooter/Assets/Scripts/PlayerHandler.cs
+++ My Top Down Shooter/Assets/Scripts/PlayerHandler.cs
@@ -5,18 +5,31 @@ using UnityEngine;
 public class PlayerHandler : MonoBehaviour
 {
     Vector2 inputVector = Vector2.zero;
+    Vector2 mouseVector = Vector2.zero;
     Controller playerController;
+    private bool aiming;
     // Start is called before the first frame update
     void Start()
     {
         playerController = GetComponent<Controller>();
+        aiming = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        inputVector.x = Input.GetAxis("Horizontal");
-        inputVector.y = Input.GetAxis("Vertical");
-        playerController.SetInputVector(inputVector);
+        if(Input.GetMouseButton(1))
+        {
+            inputVector = Vector2.zero;
+            mouseVector = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            aiming = true;
+        }
+        else{
+            inputVector.x = Input.GetAxis("Horizontal");
+            inputVector.y = Input.GetAxis("Vertical");
+            mouseVector = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            aiming = false;
+        }
+            playerController.SetVectors(inputVector, mouseVector, aiming);
     }
 }
